9c32b1144bbc6f1c52b25ab401345cde9e54e73b
Ed Courtney <edcourtney74@gmail.com>
2019-12-08 15:48:27 -0500

    Updates

---
+++ precache-manifest.108f68e6841ba1d78c6f0f1dbcfc643f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afff386cb5672060c2a2ac20ffd2a6f",
+    "url": "/christmas-scavenger-hunt/index.html"
+  },
+  {
+    "revision": "ecd2a54a77c525b26913",
+    "url": "/christmas-scavenger-hunt/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4b1eae1000e7e5463e47",
+    "url": "/christmas-scavenger-hunt/static/css/main.aa0b0b7f.chunk.css"
+  },
+  {
+    "revision": "ecd2a54a77c525b26913",
+    "url": "/christmas-scavenger-hunt/static/js/2.10e21516.chunk.js"
+  },
+  {
+    "revision": "4b1eae1000e7e5463e47",
+    "url": "/christmas-scavenger-hunt/static/js/main.b02e7ae5.chunk.js"
+  },
+  {
+    "revision": "8d7ef9639aadc9cce798",
+    "url": "/christmas-scavenger-hunt/static/js/runtime-main.98506b58.js"
+  },
+  {
+    "revision": "6f25413c5740f54bdd260e9d4829b2aa",
+    "url": "/christmas-scavenger-hunt/static/media/background.6f25413c.jpg"
+  }
+]);
